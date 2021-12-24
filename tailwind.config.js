aa78907fd248e675dd64071ad67a12809d7e8fe6
INS <j893412899@outlook.com>
2021-12-24 23:55:43 +0800

    ✨  Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'media',
+  content: [
+    './index.html',
+    './src/*.{html,js,tsx,jsx,ts}',
+    './src/**/*.{html,js,tsx,jsx,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
